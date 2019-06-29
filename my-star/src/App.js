900e5f5058dcc32fe9540ccf6e6e258dd288d2b6
Salsa <>
2019-06-28 21:19:37 -0700

    working on images and clickedCharacter logic. Not right yet

--- my-star/src/App.js
+++ my-star/src/App.js
@@ -1,71 +1,91 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Container from "./components/Container";
-//import star from "./star.json";
+import star from "./star.json";
+import StarCard from "./components/StarCard";
 import Footer from "./components/Footer";
 
 
-// class App extends Component {
-//   // Setting this.state.friends to the friends json array
-//   state = {
-//     friends
-//   };
+class App extends Component {
+  state = {
+    star,
+    clickedCharacters: [],
+    score: 0
+  };
 
-//   removeFriend = id => {
-//     // Filter this.state.friends for friends with an id not equal to the id being removed
-//     const friends = this.state.friends.filter(friend => friend.id !== id);
-//     // Set this.state.friends equal to the new friends array
-//     this.setState({ friends });
-//   };
+  //check to see if image already picked.
+  imageClick = event => {
+    const currentCharacter = event.target.alt;
+    const characterAlreadyClicked =
+      this.state.clickedCharacters.indexOf(currentCharacter) > -1;
 
-//   // Map over this.state.friends and render a FriendCard component for each friend object
-//   render() {
-//     return (
-//       <Wrapper>
-//         <Title>Friends List</Title>
-//         {this.state.friends.map(friend => (
-//           <FriendCard
-//             removeFriend={this.removeFriend}
-//             id={friend.id}
-//             key={friend.id}
-//             name={friend.name}
-//             image={friend.image}
-//             occupation={friend.occupation}
-//             location={friend.location}
-//           />
-//         ))}
-//       </Wrapper>
-//     );
-//   }
-// }
+    //image already clicked. Start over. Score is back to zero.
+    if (characterAlreadyClicked) {
+      this.setState({
+        star: this.state.star.sort(function(a, b) {
+          return 0.5 - Math.random();
+        }),
+        clickedCharacters: [],
+        score: 0
+      });
+      // want to include an alert (character already clicked. You lose, start again!);
 
-const App = () => (
-  
-  <div>
-    <Navbar />
-    <Header />
-    <div>
-        <Container image={"././public/images/Apophis"} />
-        <Container />
-        <Container />
-        <Container />
-    </div>
-    <div>
-        <Container />
-        <Container />
-        <Container />
-        <Container />
-    </div>
-    <div>
-        <Container />
-        <Container />
-        <Container />
-        <Container />
-    </div>
-    <Footer />
-  </div>
-  
-);
+      // otherwise continue picking unique character images.
+    } else {
+      this.setState(
+        {
+          star: this.state.star.sort(function(a, b) {
+            return 0.5 - Math.random();
+          }),
+          //clicked characters are array and the concat is adding a character to the array.
+          clickedCharacters: this.state.clickedCharacters.concat(
+            currentCharacter
+          ),
+          score: this.state.score + 1
+        },
+        () => {
+
+          // if you score 12 characters you win!
+          if (this.state.score === 12) {
+            alert("Yippee! You Win!");
+            this.setState({
+              star: this.state.star.sort(function(a, b) {
+                return 0.5 - Math.random();
+              }),
+              //characters zeroed out and new game begins.
+              clickedCharacters: [],
+              score: 0
+            });
+          }
+        }
+      );
+    }
+  };
+
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <div>
+        <Navbar
+        score={this.state.score} />
+        <div className="wrapper">
+        {this.state.star.map(star => (
+          <StarCard
+            imageClick={this.imageClick}
+            id={star.id}
+            key={star.id}
+            name={star.name}
+            image={star.image}
+          />
+        ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+
 
 export default App;
