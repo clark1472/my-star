3ba2f424606d3bc68714e46bd3ec7a0cf9f02316
Salsa <>
2019-07-07 19:24:13 -0700

    Updates

---
+++ precache-manifest.338020167eecda4137c46003f939d24b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f51a53ec9073c2f69d1a72b073c43ddc",
+    "url": "/my-star/index.html"
+  },
+  {
+    "revision": "2a19a28de496e9681cc2",
+    "url": "/my-star/static/css/main.8ba3339d.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/my-star/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "2a19a28de496e9681cc2",
+    "url": "/my-star/static/js/main.1bfc5437.chunk.js"
+  },
+  {
+    "revision": "48f6a555190fc8084446",
+    "url": "/my-star/static/js/runtime~main.1dff242d.js"
+  },
+  {
+    "revision": "c6a9db02e24f4d21842d25795f016cd6",
+    "url": "/my-star/static/media/Apophis.c6a9db02.jpg"
+  },
+  {
+    "revision": "fbaa3835922a4f4a5e4b633ad439c38d",
+    "url": "/my-star/static/media/BraTac.fbaa3835.jpg"
+  },
+  {
+    "revision": "97620e1a0506481f61ac6af64f0a6b75",
+    "url": "/my-star/static/media/DanielJackson.97620e1a.jpg"
+  },
+  {
+    "revision": "3a05a371d3b1ec9f7a504879d4df79b4",
+    "url": "/my-star/static/media/GeorgeHammond.3a05a371.jpg"
+  },
+  {
+    "revision": "4a69f6181c1e305f264bbfc53371db5a",
+    "url": "/my-star/static/media/JackONeill.4a69f618.jpg"
+  },
+  {
+    "revision": "6d76c8681122f87fcd6fd25319286e89",
+    "url": "/my-star/static/media/JacobCarter.6d76c868.jpg"
+  },
+  {
+    "revision": "320dc4c6acb8e63c65832727c30c0bbe",
+    "url": "/my-star/static/media/JonasQuinn.320dc4c6.jpg"
+  },
+  {
+    "revision": "721812bfdf63bf218ec75ad9d709f5eb",
+    "url": "/my-star/static/media/SamanthaCarter.721812bf.jpg"
+  },
+  {
+    "revision": "01fcca02ddad80440327c02b9c37a597",
+    "url": "/my-star/static/media/ShaRe.01fcca02.jpg"
+  },
+  {
+    "revision": "493ca32441d3de64b12f91141c8d786c",
+    "url": "/my-star/static/media/TealC.493ca324.jpg"
+  },
+  {
+    "revision": "45475fdc2eecf0b052fcb3a1b658ae1a",
+    "url": "/my-star/static/media/ValMalDoran.45475fdc.jpg"
+  },
+  {
+    "revision": "c2ae99e046a31f13f5d27167a7608dd7",
+    "url": "/my-star/static/media/stargateLogo.c2ae99e0.jpg"
+  }
+]);
